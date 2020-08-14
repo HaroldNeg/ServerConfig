caeb7ec242ac737713f65c4e63feb0c9a2a7e6e5
Harold Negrete <negreteharold@gmail.com>
2020-08-14 12:25:03 -0500

    Initial commit

---
+++ src/test/java/com/inventario/config/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
